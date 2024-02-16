95adfe236a12d600f0c545c9537684bcf68fb240
snilsnil <heo6601@naver.com>
2024-02-17 02:48:02 +0900

    alpha version

--- app.js
+++ app.js
@@ -115,6 +115,10 @@ router.use((req, res, next) => {
 
 router.get("/", pagesController.showHome); // 홈 페이지 위한 라우트 추가
 router.get("/about", pagesController.showAbout); // 코스 페이지 위한 라우트 추가
+router.get("/address", pagesController.showAddress);
+router.get("/game", pagesController.showGame);
+router.get("/onedayclass", pagesController.showOneDayClass);
+router.get("/drinkonetime", pagesController.showDrinkOneTime);
 router.get("/transportation", pagesController.showTransportation); // 교통수단 페이지 위한 라우트 추가
 
 router.get("/users/login", usersController.login);
@@ -128,36 +132,6 @@ router.get("/", pagesController.showHome); // 홈 페이지 위한 라우트 추
 router.get("/about", pagesController.showAbout); // 코스 페이지 위한 라우트 추가
 router.get("/transportation", pagesController.showTransportation); // 교통수단 페이지 위한 라우트 추가
 
-/**
- * Subscribers
- */
-router.get(
-  "/subscribers",
-  subscribersController.index,
-  subscribersController.indexView
-); // index 라우트 생성
-router.get("/subscribers/new", subscribersController.new); // 생성 폼을 보기 위한 요청 처리
-router.post(
-  "/subscribers/create",
-  subscribersController.create,
-  subscribersController.redirectView
-); // 생성 폼에서 받아온 데이터의 처리와 결과를 사용자 보기 페이지에 보여주기
-router.get(
-  "/subscribers/:id",
-  subscribersController.show,
-  subscribersController.showView
-);
-router.get("/subscribers/:id/edit", subscribersController.edit); // viewing을 처리하기 위한 라우트 추가
-router.put(
-  "/subscribers/:id/update",
-  subscribersController.update,
-  subscribersController.redirectView
-); // 편집 폼에서 받아온 데이터의 처리와 결과를 사용자 보기 페이지에 보여주기
-router.delete(
-  "/subscribers/:id/delete",
-  subscribersController.delete,
-  subscribersController.redirectView
-);
 
 /**
  * Users
@@ -200,54 +174,6 @@ router.delete(
   "/courses/:id/delete",
   coursesController.delete,
   coursesController.redirectView
-);
-
-/**
- * Talks
- */
-// router.get("/talks", talksController.index, talksController.indexView); // 모든 토크를 위한 라우트 추가
-// router.get("/talk/:id", talksController.show, talksController.showView); // 특정 토크를 위한 라우트 추가
-router.get("/talks", talksController.index, talksController.indexView); // index 라우트 생성
-router.get("/talks/new", talksController.new); // 생성 폼을 보기 위한 요청 처리
-router.post(
-  "/talks/create",
-  talksController.create,
-  talksController.redirectView
-); // 생성 폼에서 받아온 데이터의 처리와 결과를 사용자 보기 페이지에 보여주기
-router.get("/talks/:id", talksController.show, talksController.showView);
-router.get("/talks/:id/edit", talksController.edit); // viewing을 처리하기 위한 라우트 추가
-router.put(
-  "/talks/:id/update",
-  talksController.update,
-  talksController.redirectView
-); // 편집 폼에서 받아온 데이터의 처리와 결과를 사용자 보기 페이지에 보여주기
-router.delete(
-  "/talks/:id/delete",
-  talksController.delete,
-  talksController.redirectView
-);
-
-/**
- * Trains
- */
-router.get("/trains", trainsController.index, trainsController.indexView); // index 라우트 생성
-router.get("/trains/new", trainsController.new); // 생성 폼을 보기 위한 요청 처리
-router.post(
-  "/trains/create",
-  trainsController.create,
-  trainsController.redirectView
-); // 생성 폼에서 받아온 데이터의 처리와 결과를 사용자 보기 페이지에 보여주기
-router.get("/trains/:id", trainsController.show, trainsController.showView);
-router.get("/trains/:id/edit", trainsController.edit); // viewing을 처리하기 위한 라우트 추가
-router.put(
-  "/trains/:id/update",
-  trainsController.update,
-  trainsController.redirectView
-); // 편집 폼에서 받아온 데이터의 처리와 결과를 사용자 보기 페이지에 보여주기
-router.delete(
-  "/trains/:id/delete",
-  trainsController.delete,
-  trainsController.redirectView
 );
 
 /**
